2ba409a71ce2cdded03427b5ca984a8311441b0e
matthew <matthewdunbar11@gmail.com>
2017-03-11 12:29:37 -0500

    Adds DbSets for models on context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -22,5 +22,9 @@ namespace WebApplication.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Need> Needs { get; set; }
+        public DbSet<Occupant> Occupants { get; set; }
     }
 }
