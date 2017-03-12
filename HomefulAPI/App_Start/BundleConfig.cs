698e0e9e3564ac4c9c12e1ecce057e02d0dd4bbf
matthewdunbar11@gmail.com <matthewdunbar11@gmail.com>
2017-03-12 03:04:30 -0400

    working commit

--- HomefulAPI/App_Start/BundleConfig.cs
+++ HomefulAPI/App_Start/BundleConfig.cs
@@ -20,9 +20,20 @@ namespace HomefulAPI
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/materialdesignjs", "https://code.getmdl.io/1.3.0/material.min.js").Include(
+                    "~/Scripts/material.min.js"));
+            bundles.Add(new StyleBundle("~/bundles/materialdesigncss", "https://code.getmdl.io/1.3.0/material.blue-cyan.min.css").Include(
+                    "~/Content/material.blue-cyan.min.css"));
+            bundles.Add(new StyleBundle("~/bundles/materialicon", "https://fonts.googleapis.com/icon?family=Material+Icons").Include(
+                    "~/Content/materialicon"));
+
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+           //           "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
+            bundles.UseCdn = true;
         }
     }
 }
---
+++ HomefulAPI/Controllers/PackListController.cs
@@ -0,0 +1,23 @@
+﻿using HomefulAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace HomefulAPI.Controllers
+{
+    public class PackListController : Controller
+    {
+        public ViewResult Index()
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                var camps = context.Locations.Include(l => l.Needs).ToList();
+
+                return View(camps);
+            }
+        }
+    }
+}
