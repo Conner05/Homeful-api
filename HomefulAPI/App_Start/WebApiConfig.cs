838ed9f018b832c1ff36eb2515a0a530b49bec22
matthewdunbar11@gmail.com <matthewdunbar11@gmail.com>
2017-03-11 22:49:15 -0500

    Adds JSON as default serializer for text/html

--- HomefulAPI/App_Start/WebApiConfig.cs
+++ HomefulAPI/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
@@ -14,7 +15,9 @@ namespace HomefulAPI
 
             // Web API routes
             var cors = new EnableCorsAttribute("*", "*", "*");
-            config.EnableCors(cors); 
+            config.EnableCors(cors);
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
             config.MapHttpAttributeRoutes();
 
             /*config.Routes.MapHttpRoute(
