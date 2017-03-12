b08cae8eb95f5dd91a93c15c02f0c92eb823b6bc
matthewdunbar11@gmail.com <matthewdunbar11@gmail.com>
2017-03-12 03:25:20 -0400

    Adds forms

---
+++ HomefulAPI/Controllers/MVC/NeedsController.cs
@@ -0,0 +1,37 @@
+﻿using HomefulAPI.Models;
+using HomefulAPI.ViewModels.Needs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomefulAPI.Controllers.MVC
+{
+    public class NeedsController : Controller
+    {
+        // GET: Needs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Route("locations/{id}/needs/create")]
+        public async Task<ActionResult> Create(int id)
+        {
+            CreateNeedViewModel model = new CreateNeedViewModel();
+
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                Location l = (await context.Locations.FindAsync(id));
+                model.Location = l;
+                model.LocationID = l.Id;
+            }
+
+
+            return View(model);
+        }
+    }
+}
---
+++ HomefulAPI/ViewModels/Needs/CreateNeedViewModel.cs
@@ -0,0 +1,22 @@
+﻿using HomefulAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HomefulAPI.ViewModels.Needs
+{
+    public class CreateNeedViewModel
+    {
+        [Required]
+        [Description("Need description")]
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int LocationID { get; set; }
+        public Location Location { get; set; }
+    }
+}
